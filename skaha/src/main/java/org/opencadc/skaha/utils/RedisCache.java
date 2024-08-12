f610825de1656470e1b47b71d8853b2f2e67cf8e
Abhishek Ghosh <abhishek.ghosh@thoughtworks.com>
2024-08-12 16:28:02 +0530

    [SP-4265 | Abhishek/Anuja] Added JedisCache

---
+++ skaha/src/main/java/org/opencadc/skaha/utils/RedisCache.java
@@ -0,0 +1,75 @@
+
+package org.opencadc.skaha.utils;
+
+import com.google.gson.Gson;
+import redis.clients.jedis.Jedis;
+
+import java.io.Closeable;
+import java.util.Collection;
+import java.util.List;
+
+public class RedisCache implements Closeable {
+    private final Jedis jedis;
+    private final Gson gson;
+
+    public RedisCache(String host, int port) {
+        jedis = new Jedis(host, port);
+        gson = new Gson();
+    }
+
+    public void put(String key, String value) {
+        if (key == null) throw new RuntimeException("null key");
+        try {
+            jedis.set(key, value);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
+    public void put(String key, Object value) {
+        if (value == null) throw new RuntimeException("null value");
+        String valueInString = gson.toJson(value);
+        put(key, valueInString);
+    }
+
+    public void push(String key, Collection<Object> values) {
+        String[] valuesAsArray = values.stream()
+                .map(val -> gson.toJson(val))
+                .toArray(String[]::new);
+        jedis.rpush(key, valuesAsArray);
+    }
+
+    public String get(String key) {
+        if (key == null) throw new RuntimeException("null key");
+        try {
+            return jedis.get(key);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
+    public <T> T get(String key, Class<T> className) {
+        String valueInString = get(key);
+        if (valueInString == null) return null;
+        return gson.fromJson(valueInString, className);
+    }
+
+
+    public void close() {
+        jedis.close();
+    }
+
+
+    public static void main(String[] args) {
+//        try (RedisCache jedis = new RedisCache("localhost", 6379)) {
+//
+//            jedis.put("names","Abhishek Ghosh");
+//            System.out.println(jedis.get("names"));
+//        }
+        RedisCache jedis = new RedisCache("localhost", 6379);
+        jedis.push("names", List.of("Abhishek", "Anuja", "Nitin"));
+    }
+
+}
